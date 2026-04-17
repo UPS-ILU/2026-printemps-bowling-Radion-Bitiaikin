46e943de9367e6d4622b86297d1d9fbb330e55e5
Radion-Bitiaikin <radion.bitiaikin@utoulouse.fr>
2026-04-17 14:52:51 +0200

    classe Game vide

---
+++ src/tdd/Game.java
@@ -0,0 +1,15 @@
+package tdd;
+
+public class Game {
+	public Game() {
+		
+	}
+	
+	public void roll(int n) {
+		
+	}
+	
+	public int score() {
+		return 0;
+	}
+}
---
+++ test/GameTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@Test
+	void rollTest() {
+		fail("Not yet implemented");
+	}
+	
+	void scoreTest() {
+		fail("Not yet implemented");
+	}
+
+}
